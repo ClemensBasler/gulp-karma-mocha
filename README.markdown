5d4826c6624025fc84cf26a48c6a92e4c17a5254
clemensbasler <clemens-basler@t-online.de>
2016-07-26 14:24:28 +0200

    gulp has now a default task

---
+++ README.markdown
@@ -0,0 +1,3 @@
+Trying out how to setup mocha & chai for backend tests und karma & jasmine for frontend tests
+
+You can run "gulp test-frontend" and "gulp test-backend"
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var Server = require('karma').Server;
 var mocha = require('gulp-mocha');
+var runSequence = require('run-sequence');
 
 gulp.task('test-frontend', function(done) {
   new Server({
@@ -12,4 +13,9 @@ gulp.task('test-backend', function () {
     return gulp.src('backend/test/test.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
         .pipe(mocha());
+});
+
+gulp.task('default', function(done) {
+  runSequence('test-backend', 'test-frontend');
+  done();
 });
